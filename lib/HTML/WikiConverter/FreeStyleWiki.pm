bbe758fa8b681cde564f9534e1daa28b2337b721
Yusuke Watase <ywatase@gmail.com>
2012-11-05 02:36:54 +0900

    initial commit

---
+++ lib/HTML/WikiConverter/FreeStyleWiki.pm
@@ -0,0 +1,32 @@
+package HTML::WikiConverter::FreeStyleWiki;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+HTML::WikiConverter::FreeStyleWiki -
+
+=head1 SYNOPSIS
+
+  use HTML::WikiConverter::FreeStyleWiki;
+
+=head1 DESCRIPTION
+
+HTML::WikiConverter::FreeStyleWiki is
+
+=head1 AUTHOR
+
+Yusuke Watase E<lt>ywatase@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
